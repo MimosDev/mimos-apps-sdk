c394591483f4df0c7f1fcdf66ffe1e177494c3cf
Michael Korovkin <mckoro@bu.edu>
2026-01-12 22:37:49 -0800

    remove dist

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { MimosAnalytics } from "./client";
-export { ToolCallTracker } from "./tracker";
-export { MimosFrontendAnalytics } from "./frontend-client";
-export type { MimosAnalyticsConfig, ToolCallStatus, ToolErrorType, ToolCallPayload, ToolErrorPayload, MetricsRequest, SuccessResponse, ErrorResponse, TrackToolCallOptions, TrackedCallResult, } from "./types";
-export type { MimosFrontendConfig, FrontendEventType, ButtonClickPayload, MotionSwipePayload, MotionScrollPayload, SessionStartPayload, SessionEndPayload, ElementFocusPayload, AppLoadSuccessPayload, AppLoadErrorPayload, FrontendPayload, } from "./frontend-types";
